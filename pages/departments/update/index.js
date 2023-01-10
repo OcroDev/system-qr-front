d0749e972f49e07b57756f4f2f43e7c4495e5cf1
Rohermy <rohermy.ochoa@gmail.com>
2023-01-09 22:12:25 -0400

    fix department bug

--- pages/departments/update/index.js
+++ pages/departments/update/index.js
@@ -3,8 +3,8 @@ import DepartmentList from "../../../components/container/DepartmentList";
 
 export default function departmentEdit() {
   return (
-    <>
-      <DepartmentList />;
-    </>
+    <div>
+      <DepartmentList></DepartmentList>
+    </div>
   );
 }
